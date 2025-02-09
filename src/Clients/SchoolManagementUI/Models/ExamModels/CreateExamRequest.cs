760ae4867b56b7bef44c28e89b56c34e1fe2ea85
Imanov548 <imanov548@gmail.com>
2025-02-09 20:27:02 +0400

    asdasd

---
+++ src/Clients/SchoolManagementUI/Models/ExamModels/CreateExamRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolManagementUI.Models.ExamModels;
+
+public sealed record CreateExamRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string LessonId { get; set; } = string.Empty;
+    public string StudentId { get; set; } = string.Empty;
+    public DateTime ExamDate { get; set; }
+    public int ExamResult { get; set; }
+}
