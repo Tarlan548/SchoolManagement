1040ef9e3bce12c949b1b219cb8abdd48170427d
Imanov548 <imanov548@gmail.com>
2025-02-12 17:59:52 +0400

    asd

---
+++ src/Services/Exam/Infrastructure/Exam.Persistence/Configurations/ExamConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace Exam.Persistence.Configurations;
+
+public class ExamConfiguration : IEntityTypeConfiguration<Domain.Entities.Exam>
+{
+    public void Configure(EntityTypeBuilder<Domain.Entities.Exam> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasIndex(x => x.Name, "UQ_Lesson_Name").IsUnique();
+
+        builder.Property(x => x.Id).HasDefaultValueSql("uuid_generate_v4()");
+
+        builder.Property(x => x.CreatedDate)
+            .HasDefaultValueSql("Now()");
+
+        builder.Property(x => x.IsDeleted)
+            .HasDefaultValue(false);
+
+        builder.HasQueryFilter(x => !x.IsDeleted);
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(50);
+    }
+}
---
+++ src/Services/Exam/Infrastructure/Exam.Persistence/Configurations/LessonTeacherConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Exam.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Exam.Persistence.Configurations;
+
+internal class LessonTeacherConfiguration : IEntityTypeConfiguration<Domain.Entities.LessonTeacher>
+{
+    public void Configure(EntityTypeBuilder<LessonTeacher> builder)
+    {
+        builder.HasKey(lt => new { lt.LessonId, lt.TeacherId });
+
+        builder.HasOne(lt => lt.Lesson)
+            .WithMany(l => l.LessonTeachers)
+            .HasForeignKey(lt => lt.LessonId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        builder.HasOne(lt => lt.Teacher)
+            .WithMany(t => t.LessonTeachers)
+            .HasForeignKey(lt => lt.TeacherId);
+
+        builder.HasQueryFilter(lt => !lt.Lesson.IsDeleted);
+
+        builder.ToTable("LessonTeachers");
+    }
+}
