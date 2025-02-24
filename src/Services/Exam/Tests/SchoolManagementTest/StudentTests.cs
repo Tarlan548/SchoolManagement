9ea1b2340909851fb82369a743084de2afd86035
Tarlan548 <Imanov548@gmail.com>
2025-02-25 00:21:12 +0400

    asddd

---
+++ src/Services/Exam/Tests/SchoolManagementTest/StudentTests.cs
@@ -0,0 +1,39 @@
+﻿using Exam.Domain.Entities;
+
+namespace SchoolManagementDomain.Test;
+
+public class StudentTests
+{
+    [Fact]
+    public void Student_Should_NotNull()
+    {
+        // Arrange
+        var student = new Student
+        {
+            // Act
+            FirstName = "John",
+            LastName = "Doe",
+            ClassLevel = 1
+        };
+
+        // Assert
+        Assert.NotNull(student);
+    }
+
+    [Fact]
+    public void Student_Should_TrowException_IfClassLevelIsNegative()
+    {
+        // Arrange
+        var exception = Assert.Throws<ArgumentException>(() => new Student
+        {
+            // Act
+            FirstName = "John",
+            LastName = "Doe",
+            ClassLevel = -1
+        });
+        // Act
+
+        // Assert
+        Assert.Equal("ClassLevel cannot be negative", exception.Message);
+    }
+}
